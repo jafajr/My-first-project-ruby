c29d80764a24930cd4325de773c709220e3dc56b
Josá Ailton F. Araujo Junior <joseailton.ti@gmail.com>
2020-02-18 18:38:09 -0300

    Add files via upload

--- features/passagem_com_menor_tarifa.feature
+++ features/passagem_com_menor_tarifa.feature
@@ -4,14 +4,27 @@ Funcionalidade: Passagem com menor tarifa
     Selecionar menor tarifa do dia 
     dentro do perdiodo de 2 meses
 
-    Cenario: Buscar passagem volta definida
-        Dado que é acessado a pagina de pesquisa
-        Quando preenche os campos para Buscar 
+
+    Contexto: possibilidade de realizar consulta de passagens
+        Dado que usuário possa acessar a tela de pesquisa
+
+   
+     Cenario: Buscar passagem volta definida
+        Quando preenche os campos para Busca
         E clicar em Compre Aqui
         Então é retornado a lista com passagem para o periodo
 
-        # Exemplos:
+        #Exemplos:
         # | origem | destino | ida         | volta        | pessoas |
-        # | SDU    | GRU     | 15/02/2020  | 16/04/2010   | 2       |
+        # | SDU    | GRU     | 19/02/2020  | 15/08/2010   | 2       |
+    @testes
+    Cenario: Buscar passagem volta em dois meses
+        Quando preenche os campos com volta prevista em dois meses
+        E clicar em Compre Aqui
+        Então é retornado a lista com passagem para o periodo
+
+        #Exemplos:
+        # | origem | destino | ida         | volta        | pessoas |
+        # | SDU    | GRU     | 19/02/2020  | 19/04/2010   | 2       |
+
         
-         
--- features/step_definitions/buscar_passagens_step.rb
+++ features/step_definitions/buscar_passagens_step.rb
@@ -1,48 +1,59 @@
-Dado("que é acessado a pagina de pesquisa") do
+Dado("que usuário possa acessar a tela de pesquisa") do
   visit "https://www.voegol.com.br/pt"
-end
 
-Quando("preenche os campos para Buscar") do
-  def tomorrow
-    ::Date.current.tomorrow
+  @data_ida = Date.today.succ.strftime("%d-%m-%Y")
+  @data_volta = Date.today.succ.next_month(2).strftime("%d-%m-%Y")
+
+  def busca_ida
+    # Select Origem
+    find(".division-input-origin").click
+    find("input[name=header-chosen-origin]").set "SDU"
+    find(".active-result").click
+
+    # Select Destino
+    find(".chosen-placeholder-destiny").click
+    find("input[name=header-chosen-destiny]").set "GRU"
+    find(".active-result").click
+
+    # A data atual já esta selecionada,
+    # caso deseje inserir outra data, apague a atual.
+
+    # Select data atual+1
+    find("#datepickerGo").send_keys [:control, "a"], :space #limpa o campo
+    find("#datepickerGo").send_keys @data_ida, :enter
   end
 
-  # Select Origem
-  find(".division-input-origin").click
-  find("input[name=header-chosen-origin]").set "SDU"
-  find(".active-result").click
+  def passageiros
+    #Selecionando passageiros
+    find("#number-adults").send_keys [:control, "a"], :space #limpa o campo
+    find("#number-adults").send_keys("2", :enter)
+  end
+end
 
-  # Select Destino
-
-  find(".chosen-placeholder-destiny").click
-  find("input[name=header-chosen-destiny]").set "GRU"
-  find(".active-result").click
-
-  # Preencha a data de ida com as barras, no formato
-  # DDMMAAAA, depois pressione ENTER.
-  # A data atual já esta selecionada,
-  # caso deseje inserir outra data, apague a atual.
-
-  # Select data atual+1
-
-  data_ida = Date.today.strftime("%d-%m-%Y")
-
-  find("#datepickerGo").send_keys [:control, "a"], :space #limpa o campo
-  find("#datepickerGo").send_keys data_ida, :enter
-  sleep 5
+Quando("preenche os campos para Busca") do
+  puts busca_ida
 
   # Select volta definida
   find("#datepickerBack").send_keys [:control, "a"], :space #limpa o campo
   find("#datepickerBack").send_keys("15082020", :enter)
 
-  #Selecionando passageiros
-  find("#number-adults").send_keys [:control, "a"], :space #limpa o campo
-  find("#number-adults").send_keys("2", :enter)
+  puts passageiros
+end
+
+Quando("preenche os campos com volta prevista em dois meses") do
+  puts busca_ida
+
+  # Select volta definida para 2 meses a partir da ida
+  find("#datepickerBack").send_keys [:control, "a"], :space #limpa o campo
+  find("#datepickerBack").send_keys(@data_volta, :enter)
+  sleep 3
+
+  puts passageiros
 end
 
 Quando("clicar em Compre Aqui") do
   click_button "Compre aqui"
-  sleep 6
+  sleep 3
 end
 
 Então("é retornado a lista com passagem para o periodo") do
